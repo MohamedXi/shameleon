c1312ddefa374a7f1bf1302ab8e7b9e845c14e11
Ismael MOHAMED <i.mohamed@PC516.local>
2025-02-13 17:03:04 +0100

    feat: update package version, add license and keywords, and enhance welcome message display

--- src/bin/showWelcomeMessage.ts
+++ src/bin/showWelcomeMessage.ts
@@ -1,18 +1,30 @@
+#!/usr/bin/env node
+
 import figlet from 'figlet';
+import { Logger } from '../lib/logger.js';
 
 function showWelcomeMessage() {
+  process.stdout.write('Generating Shameleon logo...\n');
   figlet('Shameleon', (err: Error | null, data: string | undefined) => {
     if (err) {
-      console.log('Error generating logo:', err);
+      process.stdout.write('Error generating logo: ' + err + '\n');
       return;
     }
 
     if (data) {
-      console.log(data);
+      process.stdout.write(data + '\n');
     }
 
-    console.log('\n🦎 Shameleon: Flexible Environment Switcher CLI\n');
-    console.log('Use the --help flag to display available commands.\n');
+    process.stdout.write(`
+      =========================================
+         🦎 Welcome to Shameleon CLI! 🎉
+      =========================================
+
+      ✅ Installation complete!
+      👉 Run 'shameleon --help' to get started.
+
+    `);
+    Logger.info('Welcome message displayed.');
   });
 }
 
